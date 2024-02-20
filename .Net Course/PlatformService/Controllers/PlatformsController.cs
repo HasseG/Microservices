102ae3b6bc9869e4d4f4e778750d971b8396a64f
HasseG <hassegregers@gmail.com>
2024-02-20 13:39:17 +0100

    Added controller

---
+++ .Net Course/PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,34 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+
+namespace PlatformService.Controllers
+{
+    //[controller] Tager navnet på klassen og sletter "controller". Route = api/Platforms
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repo;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repo, IMapper mapper)
+        {
+            _repo = repo; 
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            //For debugging
+            Console.WriteLine("--> Getting platforms...");
+
+            var paltformItems = _repo.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(paltformItems));
+        }
+    }
+
+}
