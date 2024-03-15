0c1b83d8ba2983a67d6a23c0aabaf3075467eb97
HasseG <hassegregers@gmail.com>
2024-03-15 14:54:44 +0100

    Fixed port mapping and added some logging

--- .Net Course/PlatformService/Program.cs
+++ .Net Course/PlatformService/Program.cs
@@ -2,38 +2,45 @@ using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
 using PlatformService.SyncDataServices.Http;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-builder.Services.AddDbContext<AppDbContext>
-(opt => opt.UseInMemoryDatabase("InMem"));
-
-//Dependeny Injection
-builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
-builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        builder.Services.AddDbContext<AppDbContext>
+        (opt => opt.UseInMemoryDatabase("InMem"));
+
+        //Dependeny Injection
+        builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+        builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsProduction())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        PrepDb.PrepPopulation(app);
+        
+        Console.WriteLine($"--> CommandService Endpoint {app.Configuration["CommandService"]}");
+
+        app.Run();
+    }
 }
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-PrepDb.PrepPopulation(app);
-
-app.Run();
-
